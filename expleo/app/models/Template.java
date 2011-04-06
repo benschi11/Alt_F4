f051a4cd992835b2a4fd0c0c7524871f40870895
David Fandler <david.fandler@student.tugraz.at>
2011-04-06 14:55:33 +0200

    Template Class

--- expleo/app/models/Template.java
+++ expleo/app/models/Template.java
@@ -16,12 +16,33 @@ import play.db.jpa.*;
 @Entity
 public class Template extends Model
 {
+  public HashMap templates_ = new HashMap<String, String>();
 
-  public String input_;
-
-  public Template(String input)
+  public void addCommand(String command)
   {
-    input_ = input;
+    templates_.put(command, "");
   }
 
+  public void addSubstitution(String key, String userInput)
+  {
+    templates_.put(key, userInput);
+  }
+
+  public String getValue(String command)
+  {
+    return templates_.get(command).toString();
+  }
+
+  public Template generateEmptyTemplate()
+  {
+    Template newTemplate = new Template();
+    Iterator iterator = templates_.keySet().iterator();
+    while(iterator.hasNext())
+    {
+     newTemplate.addCommand(iterator.next().toString());
+    }
+
+    return newTemplate;
+  }
+  
 }
--- expleo/test/BasicTest.java
+++ expleo/test/BasicTest.java
@@ -1,13 +1,32 @@
+
 import org.junit.*;
 import java.util.*;
 import play.test.*;
 import models.*;
 
-public class BasicTest extends UnitTest {
+public class BasicTest extends UnitTest
+{
 
-    @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(3, 1 + 1);
-    }
+  @Test
+  public void SimpleTemplateTest()
+  {
+    Template firstTemplate = new Template();
+    firstTemplate.addCommand("user_");
+    firstTemplate.addCommand("address_");
 
+    firstTemplate.addSubstitution("user_", "Hannes");
+    firstTemplate.addSubstitution("address_", "Graz");
+    firstTemplate.addSubstitution("mobile_", "123456");
+
+    Template emptyTemplate = firstTemplate.generateEmptyTemplate();
+    emptyTemplate.addSubstitution("user_", "David");
+
+    assertEquals(firstTemplate.getValue("user_"), "Hannes");
+    assertEquals(firstTemplate.getValue("address_"), "Graz");
+    assertEquals(firstTemplate.getValue("mobile_"), "123456");
+    assertEquals(emptyTemplate.getValue("user_"), "David");
+    assertEquals(emptyTemplate.getValue("address_"), "");
+    assertEquals(emptyTemplate.getValue("mobile_"), "");
+    
+  }
 }
