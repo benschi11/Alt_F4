e3b086008646cbce7fcd248c2dc1b4e6af598222
Slaven Benic <benic@student.tugraz.at>
2011-05-11 16:00:28 +0200

    Reg Finish 0.9

--- expleo/app/controllers/Application.java
+++ expleo/app/controllers/Application.java
@@ -13,48 +13,38 @@ import play.data.validation.Required;
 
 import play.data.validation.Required;
 
-public class Application extends Controller
-{
-    public static void index()
-    {
-     render();
+public class Application extends Controller {
+
+    public static void index() {
+        render();
     }
 
-    public static void upload(String name, String description, File template)
-    {
-        if (template != null)
-        {
-            if (Template.upload(name, description, template))
-            {
+    public static void upload(String name, String description, File template) {
+        if (template != null) {
+            if (Template.upload(name, description, template)) {
                 flash.success("Template successfully uploaded.", null);
             }
         }
         render();
     }
 
-    public static void showAllTemplates()
-    {
-        try
-        {
+    public static void showAllTemplates() {
+        try {
             List<Template> all_templates = Template.findAll();
             render(all_templates);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             render("Application/upload.html");
         }
 
     }
 
-    public static void showSingleTemplate(long id)
-    {
+    public static void showSingleTemplate(long id) {
         Template template = Template.findById(id);
 
         render(template);
     }
 
-    public static void simpleLink()
-    {
+    public static void simpleLink() {
         String applicationPath = Play.applicationPath.getAbsolutePath();
         File templateFile = new File(applicationPath + "/data/test/SimpleDocument.txt");
         Map<String, String> replaceMap = new HashMap<String, String>();
@@ -66,44 +56,44 @@ public class Application extends Controller
         render(path);
     }
 
-  public static void selectedTemplate(Long id)
-  {
-    Template loadedTemplate = Template.findById(id);
- 
-    render(loadedTemplate);
-  }
+    public static void selectedTemplate(Long id) {
+        Template loadedTemplate = Template.findById(id);
 
-  public static void insertionComplete()
-  {
-    //Template template = Template.findById(id);
-
-    Map<String,String[]> map = request.params.all();
-    Template template = Template.findById(Long.decode(map.get("ID")[0]));
-
-    template.doMap(map);
-
-    Iterator iterator = template.templates_.keySet().iterator();
-
-    while(iterator.hasNext())
-    {
-      String key = (String) iterator.next();
-      System.out.println("Key: "+key+" value: "+ template.templates_.get(key));
+        render(loadedTemplate);
     }
 
-  }
+    public static void insertionComplete() {
+        //Template template = Template.findById(id);
 
-  public static void register()
-  {
-      render();
-  }
+        Map<String, String[]> map = request.params.all();
+        Template template = Template.findById(Long.decode(map.get("ID")[0]));
 
-  public static void doRegister(String email, String password,
-          String firstname, String lastname, String question, String answer)
-  {
-      User newuser = new User();
-      newuser.register(email, password, firstname, lastname, question, answer);
-      newuser.save();
-    index();
-  }
+        template.doMap(map);
 
+        Iterator iterator = template.templates_.keySet().iterator();
+
+        while (iterator.hasNext()) {
+            String key = (String) iterator.next();
+            System.out.println("Key: " + key + " value: " + template.templates_.get(key));
+        }
+
+    }
+
+    public static void register() {
+        render();
+    }
+
+    public static void doRegister(String email, String password,
+            String firstname, String lastname, String question, String answer) {
+        if (email.equals("") || password.equals("") || firstname.equals("")
+                || lastname.equals("")) {
+            if (email.indexOf("@") != -1) {
+                User newuser = new User();
+                newuser.register(email, password, firstname, lastname, question, answer);
+                newuser.save();
+                index();
+            }
+        }
+        // FALSCHE EINGABE
+    }
 }
