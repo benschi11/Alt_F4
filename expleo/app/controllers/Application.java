fde9520ca18ceebffaaf06443ad14fc39f046e26
Benedikt Neuhold <benedikt.neuhold@student.tugraz.at>
2011-06-01 15:32:47 +0200

    Fixed Copyright Information. Closes #4.

--- expleo/app/controllers/Application.java
+++ expleo/app/controllers/Application.java
@@ -1,7 +1,6 @@
 /*
- * COPYRIGHT INFORMATION
- * 
- * Developed by ALTernative + F4ntastic FOUR
+ *
+ * Copyright (C) 2011 SW 11 Inc.
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,9 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * 
- * 
- * 
  */
+
 package controllers;
 
 import java.io.File;
--- expleo/app/models/Template.java
+++ expleo/app/models/Template.java
@@ -1,9 +1,8 @@
 /*
- * COPYRIGHT INFORMATION
+ *
+ * Copyright (C) 2011 SW 11 Inc.
  * 
- * Developed by ALTernative + F4ntastic FOUR
- * 
- *  This program is free software; you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
@@ -16,14 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  * 
- * 
- * 
  */
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import java.io.File;
